ecccb9aac880679d3b9d7412c1d29e6c4303d28b
Luiz Guilherme C. Da Silva <costadasilvaluizguilherme@gmail.com>
2025-06-19 13:48:53 -0300

    Sucessfully creating binary files to use in the program

--- create_bin.c
+++ create_bin.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
 #include "include/reg.h"
+
+#define MIN_ARGS 3
+#define MAX_ARGS 4
 
 
 typedef struct {
@@ -60,9 +64,23 @@ bool get_params(command_params_t *params, int argc, char **argv){
 
     return true;
 }
+
+void gen_rand_string(char *out_buff, int n_chars) {
+
+    for (int i = 0; i < n_chars-1; i++){
+        int random;
+        do {
+            random = rand() % 124;
+        }
+        while (!(random >= 'A' && random <= 'Z') &&  !(random >= 'a' && random <= 'z'));
+
+        out_buff[i] = (char)random;
+    }
+    out_buff[n_chars-1] = '\0';
+}
 int main(int argc, char **argv){
 
-    if (argc < 3 || argc > 4){
+    if (argc < MIN_ARGS || argc > MAX_ARGS){
         printf("Foram passados %d argumentos, você precisa de pelo menos 2 e no máximo 3\n\n", argc -1 );
         help();
         return 0;
@@ -72,5 +90,41 @@ int main(int argc, char **argv){
     if (!get_params(&params, argc, argv))
         return 0;
 
+    srand(time(NULL));
+
+    FILE *fp = fopen(params.out, "w+b");
+
+    int *keys_used = NULL;
+
+    if (strcmp(params.ord, "al") == 0)
+        keys_used = (int*)calloc(params.n_regs, sizeof(int));
+
+    for (int i = 0; i < params.n_regs; i++) {
+        reg_t new_reg;
+        if (strcmp(params.ord, "asc") == 0)
+            new_reg.chave = i;
+        else if (strcmp(params.ord, "desc") == 0)
+            new_reg.chave = params.n_regs -1 - i;
+        else{
+            int k_gen;
+            do {
+                k_gen = rand() % params.n_regs;
+            }while(keys_used[k_gen] == 1);
+            keys_used[k_gen] = 1;
+            new_reg.chave = k_gen;
+        }
+
+        new_reg.dado1 = rand();
+        gen_rand_string(new_reg.dado2, 1000);
+        gen_rand_string(new_reg.dado3, 5000);
+
+        if (fwrite(&new_reg, sizeof(reg_t), 1, fp) != 1){
+            printf("Uncussefully write of register number %d\n Shutting down...\n", i+1);
+            fclose(fp);
+            return -1;
+
+        }
+
+    }
 
 }
